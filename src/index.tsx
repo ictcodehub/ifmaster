167ef59bfba13446b52993cd519acba10cd1ec5d
ictcodehub <75495701+ictcodehub@users.noreply.github.com>
2025-11-26 23:09:53 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./main";
+import App from "./Main";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
